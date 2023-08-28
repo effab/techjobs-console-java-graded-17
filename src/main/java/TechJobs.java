b3eb6dad7f8df6f984a4929ec1df09c8cc0a0208
effab <abeffan@gmail.com>
2023-08-28 14:45:38 -0500

    printjobs

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -77,7 +77,7 @@ public class TechJobs {
         Boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure, so we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
@@ -119,7 +119,16 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() >+ 1) {
+            for (int i = 0; i < someJobs.size(); i++) {
+                System.out.println("*****");
+                for(Map.Entry<String, String> job : someJobs.get(i).entrySet()) {
+                    System.out.println(job.getKey() + ": " + job.getValue());
+                }
+                System.out.println("*****\n");
+            }
+        } else {
+            System.out.println("No Results.");
+        }
     }
 }
